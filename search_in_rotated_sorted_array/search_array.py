8ba0ad3756faf638962c45313297758931f6d2fd
Darragh Ferry <darraghferry@hotmail.com>
2020-05-28 16:01:32 +0100

    search_array

--- search_in_rotated_sorted_array/search_array.py
+++ search_in_rotated_sorted_array/search_array.py
@@ -1,2 +1,24 @@
+import math
+
+
 def search(nums, target):
-    pass
+    offset = math.ceil(len(nums) / 2)
+    index = (len(nums) - offset) % len(nums)
+    look = True
+
+    while look:
+        if offset == 1:
+            look = False
+
+        if nums[index] == target:
+            return index
+        elif nums[index] > target:
+            # move backwards through the arr
+            # mod to keep the index within the length of the list
+            index = (index - offset) % len(nums)
+        elif nums[index] < target:
+            index = (index + offset) % len(nums)
+
+        offset = math.ceil(offset / 2)
+
+    return -1
